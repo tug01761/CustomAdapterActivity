b70eff6866dcb873ce72bdfb05be565328d77cb8
Mike Lauw <tug01761@temple.edu>
2019-10-07 18:21:58 -0400

    copying stuffs from previous project

--- app/src/main/java/temple/edu/customadapteractivity/MainActivity.java
+++ app/src/main/java/temple/edu/customadapteractivity/MainActivity.java
@@ -2,13 +2,56 @@ package temple.edu.customadapteractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    //spinner text list
+    String[] text={"White", "Red", "Blue", "Yellow", "Purple", "Gray", "Green", "Cyan", "Magenta", "Silver"};
+
+    //color list
+    int[] color={Color.parseColor("white"),
+            Color.parseColor("red"),
+            Color.parseColor("blue"),
+            Color.parseColor("yellow"),
+            Color.parseColor("purple"),
+            Color.parseColor("grey"),
+            Color.parseColor("green"),
+            Color.parseColor("cyan"),
+            Color.parseColor("magenta"),
+            Color.parseColor("silver")};
+    View background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        background = (View) findViewById(R.id.background);
+
+        Spinner spinner = (Spinner) findViewById(R.id.mySpinner);
+        spinner.setOnItemSelectedListener(this);
+
+        ColorAdapter colorAdapter = new ColorAdapter(getApplicationContext(), text, color);
+        spinner.setAdapter(colorAdapter);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        background.setBackgroundColor(color[position]);
+        TextView selectedText=  view.findViewById(R.id.colorText);
+        selectedText.setBackgroundColor(Color.parseColor("white"));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 }
